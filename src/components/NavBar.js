6788ac240d94ffa25d2b4fbbabb138242116f703
Rodrigo Brito <118493422+Rodrigobrit08t@users.noreply.github.com>
2024-04-09 16:51:50 -0300

    Criação da barra de navegação

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,35 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Logotipo from "../img/Logotipo.svg";
 import {Link} from 'react-router-dom';
+import { Button } from './Button';
+import './NavBar.css';
 
 function NavBar() {
     const[click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    };
+
+    useEffect(() => {
+        showButton()
+    }, []);
+
+    window.addEventListener('resize', showButton);
+
   return (
     <>
         <nav className='navbar'>
             <div className='navbar-container'>
-                <img src={Logotipo} alt="Logotipo"></img>
+                <img src={Logotipo} alt="Logotipo" height="100px" width= "100px"></img>
 
                 <div className='menu-icon' onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
@@ -28,8 +46,8 @@ function NavBar() {
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/ONG' className='nav-links' onClick={closeMobileMenu}>
-                            ONG
+                        <Link to='/Dashboard' className='nav-links' onClick={closeMobileMenu}>
+                            Dashboard
                         </Link>
                     </li>
                     <li className='nav-item'>
@@ -39,10 +57,12 @@ function NavBar() {
                     </li>
                 </ul>
 
+                {button && <Button buttonStyle='btn--outline'>Cadastre-se</Button>}
+
             </div>
         </nav>
     </>
-  )
+  );
 }
 
 export default NavBar
