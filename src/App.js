1978d687a07dc5f953367d90835adfe08518f98a
Rodrigo Brito <118493422+Rodrigobrit08t@users.noreply.github.com>
2024-04-10 18:06:06 -0300

    Criando o hero component e pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar';
+import './App.css';
+import Inicio from './components/pages/Inicio';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 
@@ -10,7 +12,7 @@ function App() {
         <Router>
             <NavBar>
                 <Routes>
-                    <Route path='/' exact></Route>
+                    <Route path='/' exact component={Inicio}></Route>
                 </Routes>
             </NavBar>
         </Router>
---
+++ src/components/HeroSection.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+
+
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <h1>Titulo bacana</h1>
+            <p>texto bacana</p>
+            <div className='hero-btns'>
+                <Button
+                    className='btns'
+                    buttonStyle='btn--outline'
+                    buttonSize='btn--large'
+                >
+                    COMEÇAR
+                </Button>
+
+            </div>
+        </div>
+
+    );
+}
+
+export default HeroSection;
