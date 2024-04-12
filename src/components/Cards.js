cb60eefcd1562d0273fe81b2a3625b6d6b9ad2a6
Rodrigo Brito <118493422+Rodrigobrit08t@users.noreply.github.com>
2024-04-12 16:58:12 -0300

    Atualizando Cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,26 +5,26 @@ import './Cards.css'
 function Cards() {
   return (
     <div className='cards'>
-        <h1>Olha esses pets!!!</h1>
+        <h1>Conheça</h1>
         <div className='cards__container'>
             <div className='cards__wrapper'>
                 <ul className='cards__items'>
                     <CardItem 
                         src='images/pet-1.jpg'
-                        text='Nome'
-                        label='...' 
+                        text='Descrição'
+                        label='Nome' 
                         path='/Adotar'
                     />
                     <CardItem 
                         src='images/pet-2.jpg'
-                        text='Nome'
-                        label='...' 
+                        text='Descrição'
+                        label='Nome' 
                         path='/Adotar'
                     />
                     <CardItem 
                         src='images/pet-3.jpg'
-                        text='Nome'
-                        label='...' 
+                        text='Descrição'
+                        label='Nome' 
                         path='/Adotar'
                     />
                 </ul>
