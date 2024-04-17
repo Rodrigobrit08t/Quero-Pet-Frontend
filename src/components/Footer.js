de0eb1c7223169edd4e4a3b392a390ecca476e0b
Rodrigo Brito <118493422+Rodrigobrit08t@users.noreply.github.com>
2024-04-17 17:50:25 -0300

    Desenvolvendo o Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,36 @@
-import React,{Component} from 'react';
+import React from 'react'
+import './Footer.css';
+import Logotipo from "../img/Logotipo.svg";
+import { Link } from 'react-router-dom';
 
-class Footer extends Component{
+function Footer() {
+    return (
+        <div className='footer-container'>
 
-    constructor(props){
-        super(props);
-        this.state = {};
-    }
-
-
-    render(){
-        return(
-            <div>
-                <p>contato: queropet@gmail.com</p>
+            <div class='footer-links'>
+                <div className='footer-link-wrapper'>
+                    <div class='footer-link-items'>
+                        <h2>Sobre Nós</h2>
+                        <Link to='/Sobre'>Sobre</Link>
+                        <Link to='/'>Propósito</Link>
+                        <Link to='/'>Produto</Link>
+                        <Link to='/'>Termos de Uso</Link>
+                    </div>
+                </div>
             </div>
-
-        );
-    }
+            <section class='bottom'>
+                <div class='bottom-wrap'>
+                    <div class='footer-logo'>
+                        <Link to='/' className='social-logo'>
+                            <img src={Logotipo} alt="Logotipo" height="100px" width="100px"></img>
+                            <i class='fab fa-typo3' />
+                        </Link>
+                    </div>
+                    <small class='website-rights'>Quero Pet © 2024</small>
+                </div>
+            </section>
+        </div>
+    )
 }
 
-export default Footer;
+export default Footer
--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
+import Footer from '../Footer';
 
 function Inicio(){
     return(
         <>
             <HeroSection/>
             <Cards/>
+            <Footer/>
         </>
     );
 }
